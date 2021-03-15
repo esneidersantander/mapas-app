88d40002fd8f8c42694bcec5db646d71c6ae9de3
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-03-15 10:39:56 -0500

    referencia a latitud y longitud y zoom y uso del use ref

--- src/pages/MapaPage.js
+++ src/pages/MapaPage.js
@@ -5,15 +5,18 @@ import mapboxgl from 'mapbox-gl'
 mapboxgl.accessToken = 'pk.eyJ1IjoibGltYmVydHNhbnRhbmRlciIsImEiOiJja205eDhjcGUxbWp5MnZvNHN6NnQxb2d2In0.g7cZ4mdJfSuvgS0CbT7nYA';
 
 const puntoInicial = {
-    lng:5,
-    lat:34,
-    zoom:2
+    lng:-122.4725,
+    lat:37.8010,
+    zoom:13.5
 }
 
 export const MapaPage = () => {
 
     const mapaDiv = useRef();
-    const [, setMapa] = useState();
+//    const [mapa, setMapa] = useState();
+
+    const mapa = useRef();
+    const [coords, setCoords] = useState(puntoInicial);
     useEffect(() => {
         const map = new mapboxgl.Map({
             container: mapaDiv.current,
@@ -22,12 +25,26 @@ export const MapaPage = () => {
             zoom:puntoInicial.zoom
         });
 
-            setMapa(map);
+        mapa.current = map;
     }, []);
+
+    //cuando se mueve el mapa
+    useEffect(() => {
+        mapa.current?.on('move',()=>{
+            const {lng, lat} = mapa.current.getCenter();
+            setCoords({
+                lng:lng.toFixed(4),
+                lat:lat.toFixed(4),
+                zoom: mapa.current.getZoom().toFixed(2)
+            })
+        })
+    }, [])
 
     return (
         <>
-            
+            <div className="info">
+                Lng: {coords.lng} | lat: {coords.lat} | zoom: {coords.zoom}
+            </div>
             <div
                 ref={mapaDiv}
                 className="mapContainer"
